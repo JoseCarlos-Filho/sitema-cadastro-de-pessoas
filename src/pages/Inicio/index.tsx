dd4a698ed3a60e66fe5d43d6d6e5bb9b9159911e
Ulisses <ulissessuporteti01@gmail.com>
2022-10-29 21:12:23 -0300

    criando a página de inicio

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,31 @@
+import Item from "pages/cards/Item";
+import styles from "./Inicio.module.scss";
+
+export default function Inicio() {
+    return (
+        <div className={styles.container}>
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+        </div>
+    );
+}
