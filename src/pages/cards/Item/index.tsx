2dccd2bb2aad330ebb1a14ebee263c8429a00144
Ulisses <ulissessuporteti01@gmail.com>
2022-10-30 21:07:23 -0300

    atualizando o componente Item

--- src/pages/cards/Item/index.tsx
+++ src/pages/cards/Item/index.tsx
@@ -2,14 +2,24 @@ import styles from "./Item.module.scss";
 import takeNoteIcon from "pages/cards/assets/icons/takeNote-icon.svg";
 import trashIcon from "pages/cards/assets/icons/trash-icon.svg";
 
-export default function Item() {
+export type Tprops  ={
+    id: number;
+    name: string;
+    email: string;
+    bDay: Date;
+    handleClick: ()=> void;
+};
+
+
+export default function Item({id, name, email, bDay, handleClick}:Tprops ) {
+
     return (
-        <div className={styles.infoCard}>
-            <h2 className={styles.infoCard__personName}>Name</h2>
-            <p className={styles.infoCard__personEmail}>Loremipsum@email.com</p>
-            <p className={styles.infoCard__personBDay}>29/10/2022</p>
-            <span className={styles.infoCard__btnUpdate}><img src={takeNoteIcon} alt="icone bloco de anotações" /></span>
-            <span className={styles.infoCard__btnDelete}><img src={trashIcon} alt="icone leixeira" /></span>
+        <div className={styles.infoCard} id={id.toString()}>
+            <h2 className={styles.infoCard__personName}>{name}</h2>
+            <p className={styles.infoCard__personEmail}>{email}</p>
+            <p className={styles.infoCard__personBDay}>{bDay.toString()}</p> 
+            <span className={styles.infoCard__btnUpdate} role="button" onClick={handleClick}><img src={takeNoteIcon} alt="icone bloco de anotações" /></span>
+            <span className={styles.infoCard__btnDelete} role="button" onClick={handleClick}><img src={trashIcon} alt="icone leixeira" /></span>
         </div>
     );
 };
