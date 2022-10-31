9a4b86ac7c2ffdcaf8656ac4fb0bac67372a6093
Ulisses <ulissessuporteti01@gmail.com>
2022-10-30 21:09:48 -0300

    renderizando os compenentes a partir da API

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,31 +1,43 @@
 import Item from "pages/cards/Item";
+import { useEffect, useState } from "react";
+import {IPesssoasDados, PessoasDados } from "services/api/pessoas/PessoasDados";
 import styles from "./Inicio.module.scss";
 
+
 export default function Inicio() {
+    const [registros, setRegistros] = useState<IPesssoasDados[]>([]);
+
+    useEffect(() => {
+
+        /* pega os dados da API para montar os cards */
+        PessoasDados.getAllPersons().then((result) => {
+            if (result instanceof Error) {
+                alert(result.message);
+                return;
+            } else {
+                setRegistros(result.data);
+                return;
+            }
+        });
+    }, []);
+
+    console.dir(registros);
     return (
         <div className={styles.container}>
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {
+            registros.map(
+                ({ id, name, email, bDay }): JSX.Element => {
+                return (
+                    <Item
+                        key={id}
+                        id={id}
+                        name={name}
+                        email={email}
+                        bDay={bDay}
+                        handleClick={() => console.log("ok")}
+                    />
+                );
+            })}
         </div>
     );
 }
