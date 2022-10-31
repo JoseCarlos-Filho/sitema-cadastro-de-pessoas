b56170002ad7554362c73ef4d4658c8225b4d89e
Ulisses <ulissessuporteti01@gmail.com>
2022-10-31 20:20:42 -0300

    adicionando um id para os botões dos cards

--- src/pages/cards/Item/index.tsx
+++ src/pages/cards/Item/index.tsx
@@ -2,7 +2,7 @@ import styles from "./Item.module.scss";
 import takeNoteIcon from "pages/cards/assets/icons/takeNote-icon.svg";
 import trashIcon from "pages/cards/assets/icons/trash-icon.svg";
 
-export type Tprops  ={
+export type Tprops  = {
     id: number;
     name: string;
     email: string;
@@ -18,8 +18,8 @@ export default function Item({id, name, email, bDay, handleClick}:Tprops ) {
             <h2 className={styles.infoCard__personName}>{name}</h2>
             <p className={styles.infoCard__personEmail}>{email}</p>
             <p className={styles.infoCard__personBDay}>{bDay.toString()}</p> 
-            <span className={styles.infoCard__btnUpdate} role="button" onClick={handleClick}><img src={takeNoteIcon} alt="icone bloco de anotações" /></span>
-            <span className={styles.infoCard__btnDelete} role="button" onClick={handleClick}><img src={trashIcon} alt="icone leixeira" /></span>
+            <span className={styles.infoCard__btnUpdate} id="takeNoteButton" role="button" onClick={handleClick}><img src={takeNoteIcon} alt="icone bloco de anotações" /></span>
+            <span className={styles.infoCard__btnDelete} id="trashButton" role="button" onClick={handleClick}><img src={trashIcon} alt="icone lixeira" /></span>
         </div>
     );
 };
