e8955fca20a5c56a096951b6add0ef7481548d90
Ulisses <ulissessuporteti01@gmail.com>
2022-11-01 15:53:08 -0300

    funções para auxiliar na converção de datas

---
+++ src/utils/date-tools.ts
@@ -0,0 +1,26 @@
+/* formata a data para Day(dia), Month(mês), Year(ano)*/
+const formatDateDMY = (bDay: Date) => {
+    const date = new Date(bDay);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${(day < 10 && "0" + day) || day}/${
+        (month < 10 && "0" + month) || month
+    }/${year}`;
+};
+
+// yyyy/mm/dd
+const formatDateYMD = (bDay: Date) => {
+    const date = new Date(bDay);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}-${(day < 10 && "0" + month) || month}-${
+        (day < 10 && "0" + day) || day
+    }`;
+};
+
+export const dateTools = {
+    formatDateDMY,
+    formatDateYMD,
+};
