aec0ee84d68522c68747db6b94223a311c5613a3
Ulisses <ulissessuporteti01@gmail.com>
2022-11-17 11:40:29 -0300

    adicionando uma animação de loading

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,83 +1,101 @@
 import FormUpdate from "pages/forms/FormUpdate";
 import Card from "pages/Inicio/Card";
 import { useEffect, useState } from "react";
-import {IPesssoasDados, PessoasDados } from "services/api/pessoas/PessoasDados";
+import {
+  IPesssoasDados,
+  PessoasDados,
+} from "services/api/pessoas/PessoasDados";
 import styles from "./Inicio.module.scss";
 
-
-
-
 export default function Inicio() {
-    const [apiData, setApiData] = useState<IPesssoasDados[]>([]);
-    const [render, setRender] = useState<boolean>(false);
-    const [Unmount, SetUnmount] = useState<boolean>(false);
-    const [update, setupdate] = useState<IPesssoasDados>();
+  const [apiData, setApiData] = useState<IPesssoasDados[]>([]);
+  const [render, setRender] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [unmount, setUnmount] = useState<boolean>(false);
 
-    useEffect(() => {
-        /* pega os dados da API para montar os cards */
-        PessoasDados.getAllPersons().then((result) => {
-            if (result instanceof Error) {
-                alert(result.message);
-                return;
-            } else {
-                setApiData(result.data);
-                setRender(false);
-                return;
-            }
-        });
-    }, [render]); // renderiza a tela de acordo com o state render
+  const [update, setupdate] = useState<IPesssoasDados>();
 
-    /* deleta o registro a partir do evento de click no "botão lixeira" */
-    function deletePerson(id: string):void {
-        PessoasDados.deletePersonById(parseInt(id)).then((result) => {
-            if (result instanceof Error) {
-                alert(result.message);
-                return;
-            } else setRender(true);
-        });
-    }
+  useEffect(() => {
+    /* pega os dados da API para montar os cards */
+    setTimeout(() => {
+      PessoasDados.getAllPersons().then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+          return;
+        } else {
+          setApiData(result.data);
+          setRender(false);
+          setLoading(false);
+          return;
+        }
+      });
+    }, 500);
+  }, [render]); // renderiza a tela de acordo com o state render
 
-useEffect(()=> {<> </> }, [!Unmount]);
+  /* deleta o registro a partir do evento de click no "botão lixeira" */
+  function deletePerson(id: string): void {
+    PessoasDados.deletePersonById(parseInt(id)).then((result) => {
+      if (result instanceof Error) {
+        alert(result.message);
+        return;
+      } else setRender(true);
+    });
+  }
 
-function personData (e:any){
-    const idElemento = parseInt(e!.currentTarget.parentElement!.id)
-    const found = apiData.find(({id}) => id === idElemento);  
-    
+  useEffect(() => {
+    <> </>;
+  }, [!unmount]);
+
+  function personData(e: any) {
+    const idElemento = parseInt(e!.currentTarget.parentElement!.id);
+    const found = apiData.find(({ id }) => id === idElemento);
+
     setupdate({
-         id: found!.id,
-         name: found!.name,
-         email: found!.email,
-         bDay: found!.bDay
-        });
-}
-    
-    return (
-        <div className={styles.container}>
-            {!Unmount && (
-                <>
-                {apiData.map(({ id, name, email, bDay }): JSX.Element => {
-                    return (
-                        <Card
-                        key={id}
-                        id={id}
-                        name={name}
-                        email={email}
-                        bDay={bDay}
-                        handleDelete={(e)=> deletePerson(e.currentTarget.parentElement!.id)}    
-                        handleUpdate={(e)=> {
-                            personData(e)
-                            SetUnmount(true);
-                        }
-                        } 
-                            />
-                        );
-                    })} 
-            </>
-            )}
-                
-                {update &&( 
-                    <div>{<FormUpdate id={update.id} name={update.name} email={update.email} bDay={update.bDay} />}</div>   )  
+      id: found!.id,
+      name: found!.name,
+      email: found!.email,
+      bDay: found!.bDay,
+    });
+  }
+
+  return (
+    <div className={styles.container}>
+      {!unmount ? (
+        <>
+          {apiData.map(({ id, name, email, bDay }): JSX.Element => {
+            return (
+              <Card
+                key={id}
+                id={id}
+                name={name}
+                email={email}
+                bDay={bDay}
+                handleDelete={(e) =>
+                  deletePerson(e.currentTarget.parentElement!.id)
                 }
-        </div>
-    )
+                handleUpdate={(e) => {
+                  personData(e);
+                  setUnmount(true);
+                }}
+              />
+            );
+          })}
+        </>
+      ) : null}
+
+      {update ? (
+        <>
+          {
+            <FormUpdate
+              id={update.id}
+              name={update.name}
+              email={update.email}
+              bDay={update.bDay}
+            />
+          }
+        </>
+      ) : null}
+      {loading ? <div className={styles.loader}></div> : null}
+    </div>
+  );
 }
